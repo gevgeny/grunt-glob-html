e01049e120ccda8e52abb7a684784a01d7b0ddee
Jiayong Ou <jou@orly.ch>
2014-08-27 12:24:52 +0200

    Add configuration option for base directory to glob on

--- test/glob-html.spec.js
+++ test/glob-html.spec.js
@@ -9,6 +9,11 @@ describe('glob-html', function(){
             expected = grunt.file.read('test/expected//content.html');
         assert.equal(actual, expected);
     });
+    it('should expand glob sources with custom base drectory', function() {
+        var actual = grunt.file.read('test/tmp/custom-base/content.html'),
+            expected = grunt.file.read('test/expected/content.html');
+        assert.equal(actual, expected);
+    });
     it('should not change file without glob sources', function() {
         var actual = grunt.file.read('test/tmp/multiple-src/empty-content.html'),
             expected = grunt.file.read('test/examples/empty-content.html');
