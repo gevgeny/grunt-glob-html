81c5846bedf244e6100c65600ba6e1cbde2c697c
gevgeny <e.gluhotorenko@gmai.com>
2015-04-11 13:29:09 +0300

    change build to run jslint every time

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,11 +62,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
     grunt.loadNpmTasks('grunt-mocha-test');
 
-    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-    // plugin's task(s), then test the result.
-    grunt.registerTask('test', ['clean', 'globhtml', 'mochaTest']);
-
-    // By default, lint and run all tests.
-    grunt.registerTask('default', ['jshint', 'test']);
+    grunt.registerTask('default', ['jshint', 'clean', 'globhtml', 'mochaTest']);
 
 };
